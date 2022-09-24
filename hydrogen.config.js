e464054ae39acd7866ab72c010ca5882512ecd66
Patrick Renz Garcia <trickrenzgarcia@gmail.com>
2022-09-24 23:37:37 +0800

    storefront

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -2,8 +2,8 @@ import {defineConfig} from '@shopify/hydrogen/config';
 
 export default defineConfig({
   shopify: {
-    storeDomain: 'hydrogen-preview.myshopify.com',
-    storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
+    storeDomain: 'kapengmatapangtest.myshopify.com',
+    storefrontToken: '6163e13f82768eee6fa49f5971da63eb',
     storefrontApiVersion: '2022-07',
   },
 });
