d9ec2d6b3bdcf05bc8d28550a6379d3e68f3626a
Patrick Renz Garcia <42066235+trickrenzgarcia@users.noreply.github.com>
2022-10-15 23:53:39 +0800

    Update hydrogen.config.js

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -6,13 +6,13 @@ export default defineConfig({
     defaultLanguageCode: 'EN',
     storeDomain:
       // @ts-ignore
-      Oxygen?.env?.PUBLIC_STORE_DOMAIN || 'kapengmatapangtest.myshopify.com',
+      'kapengmatapangtest.myshopify.com',
     storefrontToken:
       // @ts-ignore
-      Oxygen?.env?.PUBLIC_STOREFRONT_API_TOKEN || '1666ad3f550daaf9bbcd77d09540a967',
+      '1666ad3f550daaf9bbcd77d09540a967',
     privateStorefrontToken:
       // @ts-ignore
-      Oxygen?.env?.PRIVATE_STOREFRONT_API_TOKEN || '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',
+      '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',
     storefrontApiVersion: '2022-07',
     // @ts-ignore
     storefrontId: Oxygen?.env?.PUBLIC_STOREFRONT_ID,
