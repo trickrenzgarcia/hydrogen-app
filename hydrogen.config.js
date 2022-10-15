f9f46a2ba411933089bb372304cbbfe027a332c3
trickrenzgarcia <trickrenzgarcia@gmail.com>
2022-10-15 23:47:14 +0800

    big changes

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -9,10 +9,10 @@ export default defineConfig({
       Oxygen?.env?.PUBLIC_STORE_DOMAIN || 'kapengmatapangtest.myshopify.com',
     storefrontToken:
       // @ts-ignore
-      Oxygen?.env?.PUBLIC_STOREFRONT_API_TOKEN,
+      Oxygen?.env?.PUBLIC_STOREFRONT_API_TOKEN || '1666ad3f550daaf9bbcd77d09540a967',
     privateStorefrontToken:
       // @ts-ignore
-      Oxygen?.env?.PRIVATE_STOREFRONT_API_TOKEN,
+      Oxygen?.env?.PRIVATE_STOREFRONT_API_TOKEN || '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',
     storefrontApiVersion: '2022-07',
     // @ts-ignore
     storefrontId: Oxygen?.env?.PUBLIC_STOREFRONT_ID,
